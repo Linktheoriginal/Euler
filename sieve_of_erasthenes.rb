f4f0de61e43552f5a3202c923f8a608bf2af3bdf
John Mark Smotherman <Linktheoriginal@users.noreply.github.com>
2016-07-10 16:22:29 -0400

    Cleanup Sieve of Erasthenes

--- sieve_of_erasthenes.rb
+++ sieve_of_erasthenes.rb
@@ -1,37 +1,22 @@
-#finds and sums all primes under a given number, using the sieve of erasthenes
-#x is the number to look under
-x = 2000000
-
-#set up a 2d array, like [[2, 0], [3, 0], [4, 0], ...]
-#the first is the number we're checking, the second is whether it's a prime
-#0 means prime, 1 means it's not.  assume primeness by default.
-i = 1
-arr = Array.new(x - 1){
-	i+=1
-	[i, 0]
-}
-i = 0
-
-#now iterate through the array, marking off all multiples of each prime we find
-#the marking pattern guarantees that the next non-marked number after a pass is a new prime
-while i < x - 1
-	if arr[i][1] == 0
-		j = arr[i][0]
-		while arr[i][0] + j < x + 1
-			arr[i + j][1] = 1
-			j = j + arr[i][0]
+#finds and sums all primes under a given number, using the Sieve of Eratosthenes
+def answer(maxInt)
+	isPrime = Array.new(maxInt + 1){true}
+	
+	for possiblePrime in 2..(maxInt / 2)
+		if isPrime[possiblePrime]
+			for multiple in ((possiblePrime * 2)..maxInt).step(possiblePrime)
+				isPrime[multiple] = false
+			end
 		end
 	end
-	i = i + 1
+
+	sum = 0
+	for prime in 2..maxInt
+		if isPrime[prime]
+			sum = sum + prime
+		end
+	end
+	print sum
 end
 
-#sum the primes
-i = 0
-sum = 0
-while i < x - 1
-	if arr[i][1] == 0
-		sum = sum + arr[i][0]
-	end
-	i = i + 1
-end
-print sum
+answer(2000000)
